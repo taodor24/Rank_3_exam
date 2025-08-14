c52a1b7625abb6d8a2c7beed1445b5c5e12c12b8
Fedor Turbin <feturbin@c1r8p3.42nice.fr>
2025-08-14 16:21:13 +0200

    newwww

--- Level one/_broken_g_n_l/get_next_line.h
+++ Level one/_broken_g_n_l/get_next_line.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -11,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-=======
->>>>>>> 41238ce80c3dcd7573a4ce1802e61578e5d943c3
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -23,15 +20,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-<<<<<<< HEAD
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-=======
-char *get_next_line(int fd);
->>>>>>> 41238ce80c3dcd7573a4ce1802e61578e5d943c3
 
 #endif
